6e11ffe147d846a3a92ea42e4c8ccec6fe3bbcb4
Kirill Drozdov <kirill.dzv@gmail.com>
2016-07-13 00:21:03 +0500

    create post, post form components

---
+++ src/post-form.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class PostForm extends Component {
+  render() {
+    return (
+      <h4>Добавление записи запрещено</h4>
+    );
+  }
+}
