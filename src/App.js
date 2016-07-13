6e6011d30699044a911d75cf2c915b389161a916
Kirill Drozdov <kirill.dzv@gmail.com>
2016-07-14 01:36:28 +0500

    show news

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import Post from './post';
 import PostForm from './post-form';
+import posts from './post-store';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <Post />
+        <Post posts={posts}/>
         <PostForm />
       </div>
     );
--- src/post.js
+++ src/post.js
@@ -3,7 +3,26 @@ import React, { Component } from 'react';
 export default class Post extends Component {
   render() {
     return (
-      <h1>Суперновость</h1>
+      <ul>
+        {
+          this.props.posts.map((post) => {
+            return(
+              <li key={post.id}>
+                <h4>{post.name}</h4>
+                <div>{post.description}</div>
+              </li>
+            );
+          })
+        }
+      </ul>
     );
   }
 }
+
+Post.propTypes = {
+  posts: React.PropTypes.arrayOf(React.PropTypes.shape({
+    id: React.PropTypes.number.isRequired,
+    name: React.PropTypes.string.isRequired,
+    description: React.PropTypes.string
+  }))
+};
